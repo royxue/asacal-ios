bb341366a913ab6c31a0e089d35136b77827560b
Lijun Xue <xljroy@gmail.com>
2015-04-14 17:25:56 +0900

    facebook sdk settings

---
+++ asacal/Bridge-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridge-Header.h
+//  asacal
+//
+//  Created by Roy Xue on 4/14/15.
+//  Copyright (c) 2015 Roy Xue. All rights reserved.
+//
+
+#ifndef asacal_Bridge_Header_h
+#define asacal_Bridge_Header_h
+
+#import <Facebook-iOS-SDK/FBSDKCoreKit/FBSDKCoreKit.h>
+#import <Facebook-iOS-SDK/FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
